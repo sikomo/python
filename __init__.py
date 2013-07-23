839f93e9d7e609f00b7a4afe784f370e32122358
sikomo <komodu59@gmail.com>
2013-07-23 06:27:33 +0200

    Initial commit
     Changes to be committed:
       (use "git rm --cached <file>..." to unstage)
    
    	new file:   .idea/.name
    	new file:   .idea/encodings.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   .idea/other.xml
    	new file:   .idea/scopes/scope_settings.xml
    	new file:   .idea/test_pycharm.iml
    	new file:   .idea/testrunner.xml
    	new file:   .idea/vcs.xml
    	new file:   .idea/workspace.xml
    	new file:   __init__.py
    	new file:   __pycache__/f1.cpython-32.pyc

---
+++ __init__.py
@@ -0,0 +1,10 @@
+__author__ = 'mohamed'
+
+from f1 import f, affiche
+
+affiche(f(1, 2, 3))
+print("toto")
+
+
+
+
---
+++ f1.py
@@ -0,0 +1,8 @@
+__author__ = 'mohamed'
+
+def f(a, b, c):
+    return sum((a,b,c))
+
+
+def affiche(resultat):
+    print(resultat)
